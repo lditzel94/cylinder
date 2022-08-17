b11dc97285d12c14173b269d00544a9ec8d40b61
mercedesscilingo <mercedes.scilingo@gmail.com>
2022-08-17 14:33:57 -0300

    Final solution

--- src/main/java/org/core/Circle.java
+++ src/main/java/org/core/Circle.java
@@ -1,4 +1,23 @@
 package org.core;
 
 public class Circle {
+
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius < 0 ? 0 : radius ;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double getArea(){
+        return radius * radius * Math.PI;
+    }
+    
 }
--- src/main/java/org/core/Cylinder.java
+++ src/main/java/org/core/Cylinder.java
@@ -1,4 +1,23 @@
 package org.core;
 
-public class Cylinder {
+public class Cylinder extends Circle {
+    private double height;
+
+    public Cylinder(double radius, double height) {
+        super(radius);
+        this.height = height < 0 ? 0 : height;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+    
+    public double getVolume() {
+        return height * getArea();
+    }
+    
 }
